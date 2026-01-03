42a75384f39039ecac689773cdabf765595d551f
Kalyani Pravin Kapade <kalyanikapade6@gmail.com>
2026-01-03 14:18:45 +0530

    Refactor fusion output processing into a function
    Refactor code to include clean_fusion_op function for processing fusion outputs.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from body_temp import norm_slope, temp_stable
-from BVP import persistent_flags, anomaly_flags, Total_scores
+from BVP import  anomaly_flags, Total_scores,timeline_anomaly_score,persistent_segments,compute_persistent
 import numpy as np
 from fusion import bvp_temp_fuse
 import json
 
 fusion_outputs=bvp_temp_fuse(norm_slope,temp_stable,Total_scores,anomaly_flags)
 fusion_outputs=np.array(fusion_outputs)
-print(fusion_outputs[0])
+#print(fusion_outputs[0])
 
 def to_python(obj):
     if isinstance(obj,(np.integer,)):
@@ -17,11 +17,15 @@ def to_python(obj):
         return bool(obj)
      
     return obj  
-
-clean_op=[]
-for row in fusion_outputs:
+def clean_fusion_op(fusion_outputs):    
+ clean_op=[]
+ for row in fusion_outputs:
     clean_op.append({k:to_python(v) for k,v in row.items()})
+ return clean_op
+
+clean_op=clean_fusion_op(fusion_outputs)
 
 with open("result.json","w") as f:
     json.dump(clean_op,f,indent=2)
+
 
